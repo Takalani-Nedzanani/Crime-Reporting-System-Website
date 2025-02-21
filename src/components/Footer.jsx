968a4742a9f3a9ae4c1f0fd935476cde769cefd3
Takalani Nedzanani <135524369+Takalani-Nedzanani@users.noreply.github.com>
2025-02-21 21:19:59 +0200

    new

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="footer">
-    <p>&copy; Crime Reporting System. All rights reserved.</p>
+    <p>&copy; Takalani Nedzanani. All rights reserved.</p>
   </footer>
 );
 
